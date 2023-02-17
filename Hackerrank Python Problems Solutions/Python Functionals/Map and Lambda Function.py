8138f94d811b89d3f8fcb22f7f9a41b463185782
Asif Ahmed <121171874+asif7707@users.noreply.github.com>
2023-02-17 19:37:59 +0600

    Create Map and Lambda Function.py

---
+++ Hackerrank Python Problems Solutions/Python Functionals/Map and Lambda Function.py
@@ -0,0 +1,13 @@
+# complete the lambda function
+cube = lambda x: pow(x, 3)
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+    fib = [0, 1]
+    for i in range(2,n):
+        fib.append(fib[i-1] + fib[i-2])
+    return fib[0:n]
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
